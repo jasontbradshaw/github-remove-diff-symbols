18b8b54b84343d935cee6da8a6341235f5cc922c
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2014-10-11 11:07:29 -0500

    Implement diff symbol trimming
    We trim continuously since the symbols like to re-appear on occasion. This is
    also just the ghetto-easy way to deal with the user expanding diff context and
    other unforseen possibilities.

--- main.js
+++ main.js
@@ -1,1 +1,40 @@
-console.log('hello, world!');
+var hasClass = function (el, className) {
+  return (' ' + el.className + ' ').indexOf(' ' + className + ' ') >= 0;
+};
+
+var trimDiffLines = function () {
+  var trimmedClassName = 'blob-code-diff-trimmed';
+
+  // get all the code blob lines that haven't been trimmed yet
+  var lines = document.querySelectorAll([
+    '.blob-code-addition:not(.' +  trimmedClassName + ')',
+    '.blob-code-context:not(.' + trimmedClassName +  ')',
+    '.blob-code-deletion:not(.' + trimmedClassName +  ')',
+    '.blob-expanded .blob-code:not(.' + trimmedClassName +  ')',
+    '.blob-expanded .blob-code > span:not(.' + trimmedClassName +  ')',
+  ].join(', '));
+
+  Array.prototype.forEach.call(lines, function (line) {
+    // trim a matching leading character from text nodes of diff lines. we trim
+    // both lines of context _and_ diff lines in order to ensure that the diff
+    // lines match up with the context lines.
+    Array.prototype.forEach.call(line.childNodes, function (node) {
+      if (node.nodeType === Node.TEXT_NODE) {
+        // if the node begins with a `+`, `-`, or single leading space, trim it
+        if (/^[-+\s]/.test(node.textContent)) {
+          node.textContent = node.textContent.slice(1);
+
+          if (!hasClass(line, trimmedClassName)) {
+            line.className += ' ' + trimmedClassName;
+          }
+        }
+      }
+    });
+  });
+};
+
+// keep trying to trim diff lines, since they like to re-appear often
+setInterval(trimDiffLines, 500);
+
+// do an initial trim to catch lines that came to us on page load
+trimDiffLines();
